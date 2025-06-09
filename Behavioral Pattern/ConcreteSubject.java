9c68a7f1c436e6c530258240847dc1fc1bd38fbc
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-09 23:29:50 +0530

    Create ConcreteSubject.java

---
+++ Behavioral Pattern/ConcreteSubject.java
@@ -0,0 +1,12 @@
+public class ConcreteSubject extends Subject {
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyObservers();
+    }
+}
