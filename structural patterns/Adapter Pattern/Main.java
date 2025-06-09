618691930cc2d5c94a6b8158e139bc159f77a751
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-10 00:10:51 +0530

    Create Main.java

---
+++ structural patterns/Adapter Pattern/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
+        audioPlayer.play("mp3", "song.mp3");
+        audioPlayer.play("mp4", "movie.mp4");
+        audioPlayer.play("vlc", "video.vlc");
+    }
+}
