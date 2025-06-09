4fc8c55afa3fac72fc11972ee151864608cf7bb1
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-09 23:59:49 +0530

    Create AddExpression.java

---
+++ Behavioral Pattern/ Interpreter Pattern/AddExpression.java
@@ -0,0 +1,12 @@
+public class AddExpression implements Expression {
+    private Expression left, right;
+
+    public AddExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int interpret() {
+        return left.interpret() + right.interpret();
+    }
+}
