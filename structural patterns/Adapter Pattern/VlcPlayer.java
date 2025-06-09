147baedc5809c591c39a1244cd43fa1197adfcec
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-10 00:09:47 +0530

    Create VlcPlayer.java

---
+++ structural patterns/Adapter Pattern/VlcPlayer.java
@@ -0,0 +1,9 @@
+public class VlcPlayer implements AdvancedMediaPlayer {
+    public void playVlc(String fileName) {
+        System.out.println("Playing VLC file: " + fileName);
+    }
+
+    public void playMp4(String fileName) {
+        // Do nothing
+    }
+}
