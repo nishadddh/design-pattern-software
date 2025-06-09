d263091450219ea38077bb23289f9455b047c765
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-10 00:10:31 +0530

    Create AudioPlayer.java

---
+++ structural patterns/Adapter Pattern/AudioPlayer.java
@@ -0,0 +1,14 @@
+public class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing MP3 file: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(fileName);
+        } else {
+            System.out.println("Invalid media type: " + audioType);
+        }
+    }
+}
