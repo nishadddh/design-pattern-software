ba47eace4745b9c371c49d572750bf3cd21cfe6c
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-10 00:07:49 +0530

    Create SimpleCoffee.java

---
+++ structural patterns/ Decorator Pattern/SimpleCoffee.java
@@ -0,0 +1,9 @@
+public class SimpleCoffee implements Coffee {
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    public double getCost() {
+        return 5.0;
+    }
+}
