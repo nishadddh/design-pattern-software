275ed9b673b489c9993f71e182527bc2f3b1676b
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2025-06-10 00:08:37 +0530

    Create MilkDecorator.java

---
+++ structural patterns/ Decorator Pattern/MilkDecorator.java
@@ -0,0 +1,13 @@
+public class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    public double getCost() {
+        return coffee.getCost() + 1.0;
+    }
+}
